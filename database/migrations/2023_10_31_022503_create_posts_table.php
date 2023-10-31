77db5a551eb25c44e6945204fcba8e2407d047a3
Muhammad Andrean Saputra <andreannproject@gmail.com>
2023-10-31 15:59:24 +0700

    create posts model & migrate

--- database/migrations/2023_10_31_022503_create_posts_table.php
+++ database/migrations/2023_10_31_022503_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('excerpt');
+            $table->text('body');
             $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
