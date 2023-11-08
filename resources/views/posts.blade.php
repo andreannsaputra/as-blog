fb856b06b2c9897de96629d22ccf1c69f8ea2f48
Muhammad Andrean Saputra <andreannproject@gmail.com>
2023-11-08 16:14:29 +0700

    database seeder

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,10 +2,12 @@
 
 @section('container')
     @foreach ($posts as $row)
-        <article class="mb-5">
+        <article class="mb-5 border-bottom">
             <h2>
-                <a href="/posts/{{ $row->slug }}">{{ $row->title }}</a>
+                <a href="/posts/{{ $row->slug }}" class="text-decoration-none">{{ $row->title }}</a>
             </h2>
+            <p>By Andre in <a href="/categories/{{ $row->category->slug }} "
+                    class="text-decoration-none">{{ $row->category->name }}</a></p>
             <p>{{ $row->excerpt }}</p>
         </article>
     @endforeach
